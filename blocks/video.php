1cba016b3f3ccc835d39bd44fcfa673a73b05942
Koichi Yamashita <k-yamashita@ver2.jp>
2014-09-20 01:32:37 +0900

    youtubeのビデオidでの指定対応

--- blocks/video.php
+++ blocks/video.php
@@ -8,6 +8,7 @@ class block_video extends block {
      */
     public function add_form_elements(\MoodleQuickForm $f) {
         $f->addElement('textarea', 'content', ke::str('embedcode'), $this->codeareaattrs);
+        $f->addHelpButton('content', 'embedcode', ke::COMPONENT);
     }
 
     /**
@@ -34,21 +35,30 @@ class block_video extends block {
      */
     public function get_content(\stdClass $block) {
         $data = unserialize($block->data);
+        $content = trim($data->content);
 
-        $o = '';
+        if (stripos($content, '<iframe') !== false)
+            return $this->tweak_iframe_params($content);
+        elseif (preg_match('/^[[:alnum:]_-]{11}$/', $content))
+            return '<iframe width="100%" height="100%" src="//www.youtube.com/embed/' . $content
+                . '" frameborder="0" allowfullscreen></iframe>';
+        else
+            return \html_writer::tag('span', ke::str('invalidvideoparam'), array('class' => 'error'));
+    }
 
+    private function tweak_iframe_params($html) {
         $dom = new \DOMDocument;
-        if (!$dom->loadHTML($data->content))
-            return $data->content;
+        if (!$dom->loadHTML($html))
+            return $html;
 
         $nodelist = $dom->getElementsByTagName('iframe');
         if ($nodelist->length) {
             $iframe = $nodelist->item(0);
             $iframe->setAttribute('width', '100%');
             $iframe->setAttribute('height', '100%');
-            $o = $dom->saveHTML($iframe);
+            return $dom->saveHTML($iframe);
         }
 
-        return $o;
+        return $html;
     }
 }
