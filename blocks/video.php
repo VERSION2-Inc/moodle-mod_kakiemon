053be418e1bb900d0ec2c4ad8ad56cf879a88408
Koichi Yamashita <k-yamashita@ver2.jp>
2014-10-19 13:13:11 +0900

    youtubeビデオサイズ変更対応

--- blocks/video.php
+++ blocks/video.php
@@ -2,6 +2,9 @@
 namespace ver2\kakiemon;
 
 class block_video extends block {
+    const DEF_WIDTH = 500;
+    const DEF_HEIGHT = 280;
+
     /**
      *
      * @param \MoodleQuickForm $f
@@ -9,6 +12,13 @@ class block_video extends block {
     public function add_form_elements(\MoodleQuickForm $f) {
         $f->addElement('textarea', 'content', ke::str('embedcode'), $this->codeareaattrs);
         $f->addHelpButton('content', 'embedcode', ke::COMPONENT);
+
+        $f->addElement('text', 'videowidth', ke::str('videowidth'), array('size' => 5));
+        $f->setType('videowidth', PARAM_INT);
+        $f->setDefault('videowidth', self::DEF_WIDTH);
+        $f->addElement('text', 'videoheight', ke::str('videoheight'), array('size' => 5));
+        $f->setType('videoheight', PARAM_INT);
+        $f->setDefault('videoheight', self::DEF_HEIGHT);
     }
 
     /**
@@ -22,7 +32,9 @@ class block_video extends block {
         $formdata = $form->get_data();
 
         $data = (object)array(
-            'content' => $formdata->content
+            'content' => $formdata->content,
+            'videowidth' => $formdata->videowidth,
+            'videoheight' => $formdata->videoheight
         );
 
         $DB->set_field(ke::TABLE_BLOCKS, 'data', serialize($data), array('id' => $block->id));
@@ -37,16 +49,30 @@ class block_video extends block {
         $data = unserialize($block->data);
         $content = trim($data->content);
 
+        if (!isset($data->videowidth))
+            $data->videowidth = self::DEF_WIDTH;
+        if (!isset($data->videoheight))
+            $data->videoheight = self::DEF_HEIGHT;
+
         if (stripos($content, '<iframe') !== false)
-            return $this->tweak_iframe_attrs($content);
-        elseif (preg_match('/^[[:alnum:]_-]{11}$/', $content))
-            return '<iframe width="100%" height="100%" src="//www.youtube.com/embed/' . $content
-                . '" frameborder="0" allowfullscreen></iframe>';
-        else
+            return $this->tweak_iframe_attrs($content, $data);
+
+        elseif (preg_match('/^([[:alnum:]_-]{11})$/', $content, $m)
+            || preg_match('!youtube\.com/watch\?v=([[:alnum:]_-]{11})!', $content, $m)) {
+            $videoid = $m[1];
+            return \html_writer::tag('iframe', '', array(
+                'src' => '//www.youtube.com/embed/' . $videoid,
+                'width' => $data->videowidth,
+                'height' => $data->videoheight,
+                'frameborder' => 0,
+                'allowfullscreen' => 'allowfullscreen'
+            ));
+
+        } else
             return \html_writer::tag('span', ke::str('invalidvideoparam'), array('class' => 'error'));
     }
 
-    private function tweak_iframe_attrs($html) {
+    private function tweak_iframe_attrs($html, \stdClass $blockdata) {
         $dom = new \DOMDocument;
         if (!$dom->loadHTML($html))
             return $html;
@@ -54,8 +80,8 @@ class block_video extends block {
         $nodelist = $dom->getElementsByTagName('iframe');
         if ($nodelist->length) {
             $iframe = $nodelist->item(0);
-            $iframe->setAttribute('width', '100%');
-            $iframe->setAttribute('height', '100%');
+            $iframe->setAttribute('width', $blockdata->videowidth);
+            $iframe->setAttribute('height', $blockdata->videoheight);
             return $dom->saveHTML($iframe);
         }
 
