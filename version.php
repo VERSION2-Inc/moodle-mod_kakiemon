96f78327d7d768bc011765ed9943f23fbcc868ed
VERSION2 山下 <k-yamashita@ver2.jp>
2015-09-08 20:07:17 +0900

    ver

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$module->version = 2015070600;
+$module->version = 2015090800;
 $module->requires = 2010112400;
 $module->component = 'mod_kakiemon';
 $module->cron = 60;
