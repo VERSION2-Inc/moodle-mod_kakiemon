1a6ad4d458ef27ef53617f6466e173f7b62d6837
VERSION2 山下 <k-yamashita@ver2.jp>
2016-04-04 11:36:56 +0900

    ver

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015102300;
+$plugin->version = 2016040400;
 $plugin->requires = 2010112400;
 $plugin->component = 'mod_kakiemon';
 $plugin->cron = 60;
